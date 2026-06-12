59b8a84bc3d2e3f265da706d971e8f88b81b6112
yamayu <it232281@trident.ac.jp>
2026-06-12 12:46:35 +0900

    とりあえずやまゆう

--- SorryImlate/LoginForm.Designer.cs
+++ SorryImlate/LoginForm.Designer.cs
@@ -33,13 +33,14 @@
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(145, 177);
+            this.button1.Location = new System.Drawing.Point(129, 156);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(67, 20);
             this.button1.TabIndex = 0;
             this.button1.Text = "ログイン";
             this.button1.UseVisualStyleBackColor = true;
@@ -47,41 +48,49 @@
             // 
             // txtUserCode
             // 
-            this.txtUserCode.Location = new System.Drawing.Point(154, 47);
+            this.txtUserCode.Location = new System.Drawing.Point(137, 41);
             this.txtUserCode.Name = "txtUserCode";
-            this.txtUserCode.Size = new System.Drawing.Size(100, 24);
+            this.txtUserCode.Size = new System.Drawing.Size(89, 22);
             this.txtUserCode.TabIndex = 1;
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(154, 92);
+            this.txtPassword.Location = new System.Drawing.Point(137, 81);
             this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(100, 24);
+            this.txtPassword.Size = new System.Drawing.Size(89, 22);
             this.txtPassword.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(69, 47);
+            this.label1.Location = new System.Drawing.Point(61, 41);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(76, 17);
+            this.label1.Size = new System.Drawing.Size(67, 15);
             this.label1.TabIndex = 3;
             this.label1.Text = "学籍番号";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(72, 92);
+            this.label2.Location = new System.Drawing.Point(64, 81);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(74, 17);
+            this.label2.Size = new System.Drawing.Size(64, 15);
             this.label2.TabIndex = 4;
             this.label2.Text = "パスワード";
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(307, 81);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 5;
+            // 
             // LoginForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 17F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(711, 397);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtPassword);
@@ -101,6 +110,7 @@
         private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
